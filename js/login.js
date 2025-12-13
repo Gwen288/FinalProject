fea419f6eeaa1d3ec7a3bc19b02f3e2602531968
Gwen288 <wendy.dwumfuor@ashesi.edu.gh>
2025-12-13 05:19:50 +0000

    added login functionality

---
+++ js/login.js
@@ -0,0 +1,50 @@
+document.getElementById("login-button").addEventListener("click", async function(event) {
+    event.preventDefault();
+
+    let email = document.getElementById("email-input").value.trim();
+    let password = document.getElementById("password-input").value.trim();
+    let remember = document.getElementById("remember-me-checkbox").checked;
+
+
+    if (!email || !password) {
+        Swal.fire({ title: "Missing Information", text: "Please fill in all fields.", icon: "warning" });
+        return;
+    }
+
+    //sends data to the login.php file
+    try {
+        let response = await fetch("../php/login_logic.php", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "same-origin",
+            body: JSON.stringify({
+                email: email,
+                password: password,
+                remember: remember
+            })
+        });
+
+        let result = await response.json();
+        console.log(result);
+
+        if (result.success) {
+            Swal.fire({
+                title: "Login Successful!",
+                text: `Welcome, ${result.username}`,
+                icon: "success",
+                timer: 1500,
+                showConfirmButton: false
+            });
+
+            setTimeout(() => {
+               window.location.href = result.redirect; 
+            }, 1500);
+        } else {
+            Swal.fire({ title: "Login Failed", text: "Invalid credentials", icon: "error" });
+        }
+
+    } catch (error) {
+        Swal.fire({ title: "Error", text: "Server error occurred", icon: "error" });
+    }
+});
+
---
+++ php/login_logic.php
@@ -0,0 +1,78 @@
+<?php
+session_start(); 
+header("Content-Type: application/json");
+
+// receive json from login.js
+$data = json_decode(file_get_contents("php://input"), true);
+
+// check if data exists
+if (!$data) {
+    echo json_encode(["success" => false, "message" => "No data received"]);
+    exit;
+}
+
+$email=trim($data["email"]??'');
+$input_password=trim($data["password"]??'');
+$remember = $data["remember"] ?? false;
+
+
+//check if input fills are empty
+if (!$email || !$input_password) {
+    echo json_encode(["success" => false, "message" => "All fields are required"]);
+    exit;
+}
+
+//connecting to the database
+require("db_connect.php");
+if ($conn->connect_error) {
+    echo json_encode(["status" => "error", "message" => "DB Connection failed: " . $conn->connect_error]);
+    exit;
+}
+
+//selecting userid,firstname,lastname and password from database
+$stmt = $conn->prepare("SELECT user_id,first_name, last_name, `password`,`role` FROM portfolioHub_users WHERE TRIM(email)=?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+
+$stmt->store_result();
+
+
+//checks if any user row was found
+if ($stmt->num_rows == 0) {
+    echo json_encode(["success" => false, "message" => "User does not exist. Try again."]);
+    exit;
+}
+
+$stmt->bind_result($user_id, $first_name, $last_name,$hash,$user_type);
+$stmt->fetch();
+
+
+if (password_verify($input_password, $hash)) {
+    // store user id and username session variables
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['username'] = $first_name . " " . $last_name;
+    $_SESSION['usertype']=$user_type;
+
+    $redirect=($user_type==='admin')?"../Views/homepage.html":"../Views/homepage.html";
+
+    if ($remember) {
+        // set cookie for 30 days
+        setcookie("user_email", $email, time() + (86400 * 30), "/");
+    } else {
+        // delete cookie if it exists by setting the expiry date one hour back
+        setcookie("user_email", "", time() - 3600, "/");
+    }
+
+    echo json_encode([
+        "success" => true,
+        "username" => $_SESSION['username'],
+        "user_id" => $_SESSION['user_id'],
+        "redirect" => $redirect
+    ]);
+} else {
+    echo json_encode(["success" => false, "message" => "Incorrect password. Try again."]);
+}
+
+$stmt->close();
+$conn->close();
+?>
